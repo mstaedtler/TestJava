376e537382a98c39d478403b7e70254cd0efef6d
Michael Städtler <Mischka86@gmail.com>
2017-09-28 20:08:03 +0200

    Test

---
+++ StrengGeheim/src/Main.java
@@ -0,0 +1,41 @@
+import java.util.Random;
+
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		for(int i=0; i <= 1000; i++){
+//			Math.r
+			Random r = new Random(1);
+			System.out.println("1 0 0 0 1 1 1 1 0 0 1 1 0 1 0 0 0 1 1 1 1 0 0 1 1 0 0 0 1 1 1 1 0 1 0 0 0 1 1 1 1 0 0 1 1 1 0 1 1 1 1 1 0 0 1 1 1 1 0 1 0 0 0 1 1 1 1 0 0 1 1 1 0 1 1 1 1 1");
+		}
+
+	}
+
+/*	
+	public static void main(String[] args)  {
+	      int array[][]= {{1,3,5},{2,4,6}};
+	    System.out.println("Row size= " + array.length);
+	    System.out.println("Column size = " + array[1].length);
+	    outputArray(array);
+	  }
+	    
+	   public static void outputArray(int[][] array) {
+	     int rowSize = array.length;
+	     int columnSize = array[0].length;
+	     for(int i = 0; i <= 1; i++) {
+	       System.out.print("[");
+	       for(int j = 0; j <= 2; j++) {
+	         System.out.print(" " + array[i][j]);
+	       }
+	       System.out.println(" ]");
+	     }
+	     System.out.println();
+	   }
+*/	
+	
+}
